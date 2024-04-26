7379cf4d2a27077679787ddb37dbd32e7d13575c
Bogdan Andrei Tutuianu <128369457+bogX2@users.noreply.github.com>
2024-04-26 15:17:20 +0200

    progrettosiwmovie

---
+++ siw-movie-02/src/main/java/it/uniroma3/siw/SiwMovie02Application.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwMovie02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwMovie02Application.class, args);
+	}
+
+}
